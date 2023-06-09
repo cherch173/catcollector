f1b1c346131ee415420620acc447c597223245e0
Cherch <b.cherchiglia@gmail.com>
2023-06-09 11:59:38 -0400

    part 2 complete

--- catcollector/main_app/urls.py
+++ catcollector/main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    # Part 10 Step 3) Define the Route
+    path('cats/', views.cats_index, name='index'),
 ]
--- catcollector/main_app/views.py
+++ catcollector/main_app/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
+# (Baby Step) Part 10 Step 4) Code the View - add the cats!
+cats = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+# Part 10 Step 4) Code the View
+def cats_index(request):
+    return render(request, 'cats/index.html', {
+        'cats': cats
+    })
