7c28e3e078cb981b6571acf57ef4b7431f8a170f
Cherch <b.cherchiglia@gmail.com>
2023-06-12 12:39:59 -0400

    part 9 admin features

--- catcollector/main_app/views.py
+++ catcollector/main_app/views.py
@@ -16,10 +16,22 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-# Part 10 Step 4) Code the View
+# VID 1 (Setup) Part 10 Step 4) Code the View
 def cats_index(request):
     # VID 2 (Models) Step 8.1 INCLUDE the ALL METHOD to INDEX FUNCTION
     cats = Cat.objects.all()
     return render(request, 'cats/index.html', {
         'cats': cats
     })
+
+# VID 2 (Models) Step 9 - ADMIN PRIVILEGES
+
+# Step 9.1 CREATE a SUPERUSER
+# in your terminal enter:
+# python3 manage.pr createsuperuser
+
+# enter username
+# enter password you can remember cause you can't see it
+
+# if you mess up use python3 manage.py changepassword <user_name>
+# to CHANGE or UPDATE your password
