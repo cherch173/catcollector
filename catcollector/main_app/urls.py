b385d62cab17998d506053309cca8ee49c8ba2c3
Cherch <b.cherchiglia@gmail.com>
2023-06-12 12:57:42 -0400

    HTML added for Details (show functionality) for Cats

--- catcollector/main_app/urls.py
+++ catcollector/main_app/urls.py
@@ -4,6 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    # VID 1 (Intro) 
     # Part 10 Step 3) Define the Route
     path('cats/', views.cats_index, name='index'),
+    # VID 2 (Models) 
+    # Step 10.1 Determine the proper route for Details Page
+    # (/cats/:id)
+    
 ]
